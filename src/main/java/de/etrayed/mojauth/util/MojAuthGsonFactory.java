f14c10539552f21d19820d2ac3d0d32b65b6eed4
Etrayed <etrayedyt@gmail.com>
2019-12-29 21:49:58 +0100

    Deserializing using Gson's new JsonDeserializer

--- src/main/java/de/etrayed/mojauth/util/MojAuthGsonFactory.java
+++ src/main/java/de/etrayed/mojauth/util/MojAuthGsonFactory.java
@@ -1,13 +1,8 @@
 package de.etrayed.mojauth.util;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.TypeAdapter;
-import com.google.gson.stream.JsonReader;
-import com.google.gson.stream.JsonToken;
-import com.google.gson.stream.JsonWriter;
+import com.google.gson.*;
 
-import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
@@ -17,133 +12,55 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class MojAuthGsonFactory {
 
-    private static final ProfileTypeAdapter PROFILE_TYPE_ADAPTER;
-
-    private static final UserInfoTypeAdapter USER_INFO_TYPE_ADAPTER;
-
-    static {
-        PROFILE_TYPE_ADAPTER = new ProfileTypeAdapter();
-        USER_INFO_TYPE_ADAPTER = new UserInfoTypeAdapter();
-    }
-
     public static Gson newInstance() {
         return new GsonBuilder().setPrettyPrinting().serializeNulls()
-                .registerTypeAdapter(Profile.class, PROFILE_TYPE_ADAPTER)
-                .registerTypeAdapter(UserInfo.class, USER_INFO_TYPE_ADAPTER).create();
+                .registerTypeAdapter(Profile.class, new ProfileDeserializer())
+                .registerTypeAdapter(UserInfo.class, new UserInfoDeserializer()).create();
     }
 
-    private static final class ProfileTypeAdapter extends TypeAdapter<Profile> {
+    private static final class ProfileDeserializer implements JsonDeserializer<Profile> {
 
         @Override
-        public void write(JsonWriter jsonWriter, Profile profile) {}
-
-        @Override
-        public Profile read(JsonReader jsonReader) throws IOException {
-            UUID id = null;
-            String name = null;
-
-            jsonReader.beginObject();
-
-            while (jsonReader.hasNext()) {
-                String entryName = nextNameOrEnd(jsonReader);
-
-                if(entryName == null) {
-                    continue;
-                }
-
-                if (entryName.equals("id")) {
-                    id = hexStringToUUID(jsonReader.nextString());
-                } else if (entryName.equals("name")) {
-                    name = jsonReader.nextString();
-                } else {
-                    jsonReader.skipValue();
-                }
+        public Profile deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext context)
+                throws JsonParseException {
+            if(!jsonElement.isJsonObject()) {
+                throw new JsonParseException("not a JsonObject");
             }
 
-            return new Profile(id, name);
+            JsonObject profileObject = jsonElement.getAsJsonObject();
+
+            return new Profile(hexStringToUUID(profileObject.get("id").getAsString()),
+                    profileObject.get("name").getAsString());
         }
     }
 
-    private static final class UserInfoTypeAdapter extends TypeAdapter<UserInfo> {
+    private static final class UserInfoDeserializer implements JsonDeserializer<UserInfo> {
 
         @Override
-        public void write(JsonWriter jsonWriter, UserInfo userInfo) {}
-
-        @Override
-        public UserInfo read(JsonReader jsonReader) throws IOException {
-            UUID id = null;
-            String username = null;
-            Map<String, String> properties = new ConcurrentHashMap<>();
-
-            jsonReader.beginObject();
-
-            while (jsonReader.hasNext()) {
-                String entryName = nextNameOrEnd(jsonReader);
-
-                if(entryName == null) {
-                    continue;
-                }
-
-                switch (entryName) {
-                    case "id":
-                        id = hexStringToUUID(jsonReader.nextString());
-                        break;
-                    case "username":
-                        username = jsonReader.nextString();
-                        break;
-                    case "properties":
-                        jsonReader.beginArray();
-
-                        while (jsonReader.peek() != JsonToken.END_ARRAY) {
-                            jsonReader.beginObject();
-
-                            String arrayEntryName = jsonReader.nextName();
-
-                            if (arrayEntryName.equals("name")) {
-                                String propertyName = jsonReader.nextString();
-
-                                jsonReader.nextName();
-
-                                properties.put(propertyName, jsonReader.nextString());
-                            } else {
-                                String propertyValue = jsonReader.nextString();
-
-                                jsonReader.nextName();
-
-                                properties.put(jsonReader.nextString(), propertyValue);
-                            }
-
-                            jsonReader.endObject();
-                        }
-
-                        jsonReader.endArray();
-                        break;
-                    default:
-                        jsonReader.skipValue();
-                        break;
-                }
+        public UserInfo deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext context)
+                throws JsonParseException {
+            if(!jsonElement.isJsonObject()) {
+                throw new JsonParseException("not a JsonObject");
             }
 
-            return new UserInfo(id, username, properties);
+            JsonObject userInfoObject = jsonElement.getAsJsonObject();
+
+            return new UserInfo(hexStringToUUID(userInfoObject.get("id").getAsString()),
+                    userInfoObject.get("username").getAsString(),
+                    jsonArrayToPropertyMap(userInfoObject.getAsJsonArray("properties")));
         }
     }
 
-    private static String nextNameOrEnd(JsonReader reader) throws IOException {
-        if(reader.peek() == JsonToken.END_OBJECT) {
-            reader.endObject();
+    private static Map<String, String> jsonArrayToPropertyMap(JsonArray array) {
+        Map<String, String> propertyMap = new ConcurrentHashMap<>();
 
-            return null;
-        }
+        array.forEach(element -> {
+            JsonObject object = element.getAsJsonObject();
 
-        String entryName = reader.nextName();
+            propertyMap.put(object.get("name").getAsString(), object.get("value").getAsString());
+        });
 
-        if(reader.peek() == JsonToken.NULL) {
-            reader.skipValue();
-
-            return null;
-        }
-
-        return entryName;
+        return propertyMap;
     }
 
     private static UUID hexStringToUUID(String str) {
